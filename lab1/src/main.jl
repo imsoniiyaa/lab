1af8ac59ae11c70a2d84347cd49b1a90db72bbbd
imsoniiyaa <sejang@gmail.com>
2026-05-19 15:07:22 -0400

    ex1

---
+++ lab1/src/main.jl
@@ -0,0 +1,16 @@
+include("LLRSVD.jl")
+include("todense.jl")
+include("toelem.jl")
+
+using .LLRSVDModule
+using .ToDenseModule
+using .ToElemModule
+using LinearAlgebra
+
+# Verification test
+W = randn(30, 20)
+A = LLRSVD(W, 0.0)
+err = norm(W - todense(A))
+
+println(err)
+println("Pass? ", err < 1e-12)
